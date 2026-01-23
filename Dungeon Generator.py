aa726eb03c518feef2e2452a01baee3b5da8b3b8
Silver532 <silver532mc@gmail.com>
2026-01-23 12:05:29 -0500

    Finished room fill and added docstrings to Dungeon Generator file

--- Dungeon Generator.py
+++ Dungeon Generator.py
@@ -3,17 +3,38 @@ Dungeon Map Generator
 """
 
 import numpy as np
+from numpy import uint8
 from numpy.typing import NDArray
 from random import randint as rand
-from Constants import *
 from os import system as sys
-
-sys("cls")
+from Constants import *
 
 def init_tilemap(size: int = 15):
-    return np.zeros((size,size), np.uint8)
+    """
+    Parameters
+    ----------
+    size : int
+        Size of tilemap.
 
-def room_fill(tilemap):
+    Returns
+    -------
+    tilemap : NDArray[uint8]
+        2D array of given size.
+    """
+    return np.zeros((size,size), uint8)
+
+def room_fill(tilemap: NDArray[uint8]):
+    """
+    Parameters
+    ----------
+    tilemap : NDArray[uint8]
+        Empty 2D array.
+
+    Returns
+    -------
+    tilemap : NDArray[uint8]
+        2D array with placed rooms of random size.
+    """
     mid = DUNGEON_SIZE//2
     for _ in range(BOX_COUNT):
         y_s, y_e = rand(1, mid - 1), rand(mid + 2, DUNGEON_SIZE - 2)
@@ -22,6 +43,12 @@ def room_fill(tilemap):
     return tilemap
 
 def main():
+    """
+    Local Handler for Dungeon Generation
+    ------------------------------------
+    Only for use when running program from file\n
+    Visualizer will be placed here once made
+    """
     tilemap = init_tilemap()
     tilemap = room_fill(tilemap)
     print(tilemap)
@@ -33,4 +60,5 @@ def main():
     return
 
 if __name__ == "__main__":
+    sys("cls")
     main()
