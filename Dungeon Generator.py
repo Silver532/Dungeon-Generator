4adcc8d1b72d3161c267420417b93207dd464df3
Silver532 <silver532mc@gmail.com>
2026-01-27 13:37:00 -0500

    Added functionality to adj_map, to allow user to determine if neighbor_map should include indices outside of tilemap

--- Dungeon Generator.py
+++ Dungeon Generator.py
@@ -43,7 +43,7 @@ def room_fill(tilemap: array[uint8]):
         tilemap[y_s:y_e, x_s:x_e] = TEMP
     return tilemap
 
-def adj_map(tilemap: array[uint8], neighbor_map: array[uint8]):
+def adj_map(tilemap: array[uint8], neighbor_map: array[uint8], iso: bool = True):
     """
     Parameters
     ----------
@@ -59,7 +59,8 @@ def adj_map(tilemap: array[uint8], neighbor_map: array[uint8]):
 
     neighbor_map[1:19, :] = tilemap[0:18, :] + tilemap[2:20, :]
     neighbor_map[:, 1:19] += tilemap[:, 0:18] + tilemap[:, 2:20]
-    neighbor_map *= tilemap
+    if iso:
+        neighbor_map *= tilemap
     return neighbor_map
 
 def room_eroder(tilemap: array[uint8]):
