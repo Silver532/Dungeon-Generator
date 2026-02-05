abb0a5dd044a6a4b3b7b8cdd8e92797cb3397c92
Silver532 <silver532mc@gmail.com>
2026-02-05 09:31:48 -0500

    Add handler function and debug timer to Room_Generator

--- Room_Generator.py
+++ Room_Generator.py
@@ -1,7 +1,24 @@
+import numpy as np
+from numpy import uint8
+from numpy.typing import NDArray as array
+
 from Constants import *
 from Generator_Helpers import *
 
+def room_map_generator() -> array[uint8]:
+    tilemap = init_tilemap(25)
+    return tilemap
+
 def _main() -> None:
+    from time import perf_counter_ns as clock
+    print("\033c", end="")
+    start_time = clock()
+
+    tilemap = room_map_generator()
+
+    end_time = clock()
+    delta_time = (end_time - start_time)/1000000
+    print(f"Program ran in {delta_time} milliseconds")
     return
 
 if __name__ == "__main__":
